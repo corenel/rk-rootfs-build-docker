00188dfcb0384bce5e91c2e9c880e4493d55be2b
corenel <xxdsox@gmail.com>
2020-04-04 13:35:52 +0800

    feat(docker): Add dockerfile

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t corenel/rkbuild:xenial -f Dockerfile .
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run --privileged -it --rm -v /media/Data/yuthon:/data corenel/rkbuild:xenial /bin/bash
